3fd9bc7396d26014c01245993b64ca0f8c66475e
Meet Mori <meet.mori@tatvasoft.com>
2025-11-07 17:53:03 +0530

    Fix comment formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // <-- EXACTLY your repo name with leading/trailing slashes
+  base: '/portfolio/', // your repo name
 })
